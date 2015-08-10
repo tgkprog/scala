70a18df0f5be88aeebc1dc06b0c516b947e0a232
Tushar Kapila <tushar.kapila@happiestminds.com>
2015-08-11 04:32:57 +0530

    script and

---
+++ academic/Script1.scala
@@ -0,0 +1,7 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+   object Script1 extends App {
+      println("A nifty script chmod +x <file> then can run it directly")
+    }
+    HelloWorld.main(args)
---
+++ academic/Start1.scala
@@ -0,0 +1,4 @@
+    object Start1 extends App {
+	var s = "beleive it";
+      println(s"This is scala, $s.")
+    }
